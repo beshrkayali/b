28b4f28ff811b75ee0efc3beb4ce24127541f8e6
beshrkayali <beshrkayali@gmail.com>
2012-11-08 18:23:01 +0200

    First commit

---
+++ fabfile.py
@@ -0,0 +1,52 @@
+from fabric.api import *
+
+def prod():
+    env.hosts = ['user@ip.ip.ip.ip']
+    env.directory = '/' # the directory of the virtualenv that contains your site on your server
+    env.activate = 'source ../bin/activate'
+
+
+def virtualenv(command):
+    with cd(env.directory):
+        run(env.activate + '&&' + command)
+
+def push_content():
+    local('git add content/.')
+    local('git commit -m "Updated content"')
+    local('git push origin master')
+
+def push_all():
+    local('git add .')
+    local('git commit -m "Pushing all updates"')
+    local('git push origin master')
+
+def pull():
+    with cd(env.directory):
+        run('git pull')
+
+def pushpull_content():
+    push_content()
+    pull()
+
+def pushpusll():
+    push_all()
+    pull()
+
+def generate():
+    with cd(env.directory):
+        virtualenv('hyde.py -g -s .')
+
+
+def pushpullgenerate():
+    pushpusll()
+    generate()
+
+def clean():
+    with cd(env.directory):
+        run('rm -rf ./deploy')
+
+
+def pushpullcleangenerate():
+    pushpusll()
+    clean()
+    generate()
---
+++ readme.md
@@ -0,0 +1,18 @@
+B, a hyde theme
+===============
+
+
+This piece of code is a [Hyde](https://github.com/lakshmivyas/hyde) theme I created for use on my [personal site](http://beshr.com). It’s trendy, clean and responsive.
+
+Here’s how it looks like:
+<img src=“http://i.imgur.com/TjMMi.png” style=“width:100%”>
+
+
+## How does it work? ##
+- Edit the settings.py file
+- Configure your content
+- Run “hyde.py -g -s .” to generate the static site 
+- There’s a fabfile.py inside to make it easier to deploy your content :) (edit the file to reflect your server config)
+
+<div style=“align:center;”>
+<a rel="license" href="http://creativecommons.org/licenses/by/3.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by/3.0/88x31.png" /></a><br />B-Theme is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/deed.en_US">Creative Commons Attribution 3.0 Unported License</a>.</div>
