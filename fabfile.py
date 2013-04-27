b044aa9903c2a3bb28b395109288f267746fc26e
beshrkayali <beshrkayali@gmail.com>
2013-04-27 16:33:43 +0300

    More typo fixes + PIP8ified fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,9 @@
 from fabric.api import *
 
+
 def prod():
     env.hosts = ['user@ip.ip.ip.ip']
-    env.directory = '/' # the directory of the virtualenv that contains your site on your server
+    env.directory = '/'  # the directory of the virtualenv that contains your site on your server
     env.activate = 'source ../bin/activate'
 
 
@@ -10,27 +11,33 @@ def virtualenv(command):
     with cd(env.directory):
         run(env.activate + '&&' + command)
 
+
 def push_content():
     local('git add content/.')
     local('git commit -m "Updated content"')
     local('git push origin master')
+
 
 def push_all():
     local('git add .')
     local('git commit -m "Pushing all updates"')
     local('git push origin master')
 
+
 def pull():
     with cd(env.directory):
         run('git pull')
+
 
 def pushpull_content():
     push_content()
     pull()
 
-def pushpusll():
+
+def pushpull():
     push_all()
     pull()
+
 
 def generate():
     with cd(env.directory):
@@ -38,8 +45,9 @@ def generate():
 
 
 def pushpullgenerate():
-    pushpusll()
+    pushpull()
     generate()
+
 
 def clean():
     with cd(env.directory):
